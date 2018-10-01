552d94ba290c208145661254a0ada473cb940955
Marie Économidès <marie.economides@gmail.com>
2018-10-01 07:37:44 +0200

    repair  svg

--- ApprocheObjetCoursMaster/td/td3/src/main/java/fr/ubordeaux/ao/SVG.java
+++ ApprocheObjetCoursMaster/td/td3/src/main/java/fr/ubordeaux/ao/SVG.java
@@ -5,29 +5,26 @@ import java.util.*;
 import org.jdom2.*;
 import org.jdom2.output.*;
 
-
 public class SVG {
     private int width;
-    private int heigth;
-    //créer une liste 
-
+    private int height;
+    // créer une liste
 
     public SVG(int width, int heigth) {
         this.width = width;
         this.heigth = heigth;
     }
 
-    public void generateHTML(Element body){
+    public void generateHTML(Element body) {
         Element svg = new Element("svg");
-        svg.setAttribute(new Attribute("width",""+width+""));
-        svg.setAttribute(new Attribute("height",""+height+""));
+        svg.setAttribute(new Attribute("width", "" + width + ""));
+        svg.setAttribute(new Attribute("height", "" + height + ""));
         body.addContent(svg);
-        
+
     }
 
-    public void add(Forme forme){
-        //ajoute la forme à la liste
+    public void add(Forme forme) {
+        // ajoute la forme à la liste
     }
-        
-    
+
 }

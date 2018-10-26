a330cadf22c255ff069d0cb92104dfc1f6036aa7
Marie Economides <meconomides@emi.u-bordeaux1.fr>
2018-10-26 11:54:40 +0200

    td6

---
+++ ApprocheObjetCoursMaster/td/td6/src/main/java/fr/ubordeaux/ao/application/command/AddSubCatalog.java
@@ -0,0 +1,16 @@
+//implemente l'interface java
+
+package fr.ubordeaux.ao.application.command;
+
+public class AddSubCatalog extends Command{
+    private Catalog catalog;
+    private Reference reference;
+    public AddReference(Catalog catalog, Reference reference){
+        this.catalog=catalog;
+        this.reference=reference;
+    }
+    public void execute(){
+        catalog.add(reference);
+    }
+    //2 setteur si on veut changer le catalog ou la reference
+}
